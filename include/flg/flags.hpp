7ab5b2c9379b10b0ff83043ea0a99f6bf4ff7d87
Tobias Widlund <widlundtobias@gmail.com>
2018-11-17 17:42:59 +0100

    improve implementation

--- include/flg/flags.hpp
+++ include/flg/flags.hpp
@@ -2,68 +2,98 @@
 
 namespace flg
 {
-template <typename numeric_type, typename tag>
-struct flag
-{
-
-struct type
+template <typename numeric_type, typename user_type>
+struct flags
 {
     using value_type = numeric_type;
 
-    explicit operator value_type() const { return value; }
-    operator bool() const { return value; }
+    explicit constexpr operator value_type() const { return value; }
+    constexpr operator bool() const { return value; }
 
-    type operator~() const
+    constexpr flags operator~() const
     {
-        return type{~value};
+        return flags{~value};
     }
-    type operator&(type other) const
+    constexpr flags operator&(flags other) const
     {
-        return type{value & other.value};
+        return flags{value & other.value};
     }
-    type operator|(type other) const
+    constexpr flags operator|(flags other) const
     {
-        return type{value | other.value};
+        return flags{value | other.value};
     }
-    type operator^(type other) const
+    constexpr flags operator^(flags other) const
     {
-        return type{value ^ other.value};
+        return flags{value ^ other.value};
     }
-    type& operator&=(type other)
+    constexpr flags& operator&=(flags other)
     {
         value &= other.value;
         return *this;
     }
-    type& operator|=(type other)
+    constexpr flags& operator|=(flags other)
     {
         value |= other.value;
         return *this;
     }
-    type& operator^=(type other)
+    constexpr flags& operator^=(flags other)
     {
         value ^= other.value;
         return *this;
     }
-    bool operator==(type other) const
+    constexpr bool operator==(flags other) const
     {
         return value == other.value;
     }
-    bool operator!=(type other) const
+    constexpr bool operator!=(flags other) const
     {
         return value != other.value;
     }
 
     value_type value;
 
-    constexpr static type none_flag()
+    struct flag
+    {
+        value_type value;
+
+        constexpr operator user_type() const
+        {
+            return user_type{value};
+        }
+        constexpr user_type operator~() const
+        {
+            return user_type{~value};
+        }
+        constexpr user_type operator&(flag other) const
+        {
+            return user_type{value & other.value};
+        }
+        constexpr user_type operator|(flag other) const
+        {
+            return user_type{value | other.value};
+        }
+        constexpr user_type operator^(flag other) const
+        {
+            return user_type{value ^ other.value};
+        }
+        constexpr bool operator==(flag other) const
+        {
+            return value == other.value;
+        }
+        constexpr bool operator!=(flag other) const
+        {
+            return value != other.value;
+        }
+    };
+
+    constexpr static flag none_flag()
     {
        return  {0};
     }
-    
-    constexpr static type from_index(value_type v)
+
+    constexpr static flag from_index(value_type v)
     {
         return {1 << v};
     }
-};
 };
 }
